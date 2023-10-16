1058de693ba73cc228eaa6f1b55923f2fde4c9b3
MatthewChang908 <115748886+MatthewChang908@users.noreply.github.com>
2023-10-16 16:48:53 -0500

    Add database

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css';
-import Banner from './components/Banner';
-import CourseList from './components/CourseList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useJsonQuery } from './utilities/fetch';
-import { useState } from 'react';
-import TermSelector from './components/TermSelector';
+import { useDbData } from './utilities/firebase';
 import Dispatcher from './components/Dispatcher';
 const Main = () => {
-  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  const [data, error] = useDbData('/');
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading user data...</h1>;
+  if (data === undefined) return <h1>Loading user data...</h1>;
   if (!data) return <h1>No user data found</h1>;
-  if (!data.courses) return <h1>No courses found</h1>;
 
   
   return (
---
+++ src/utilities/firebase.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, onValue, ref, update} from 'firebase/database';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0y0AMDtcfu1M5yvMA99IVZrHesEcN6Zw",
+  authDomain: "quick-react-c605f.firebaseapp.com",
+  databaseURL: "https://quick-react-c605f-default-rtdb.firebaseio.com",
+  projectId: "quick-react-c605f",
+  storageBucket: "quick-react-c605f.appspot.com",
+  messagingSenderId: "148332390271",
+  appId: "1:148332390271:web:3d46f21a796717daa0dc4a",
+  measurementId: "G-5GWN5N3WHS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+export { database }
+
+export const useDbData = (path) => {
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+  
+    useEffect(() => (
+      onValue(ref(database, path), (snapshot) => {
+       setData( snapshot.val() );
+      }, (error) => {
+        setError(error);
+      })
+    ), [ path ]);
+  
+    return [ data, error ];
+  };
+  
+  const makeResult = (error) => {
+    const timestamp = Date.now();
+    const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+    return { timestamp, error, message };
+  };
+  
+  export const useDbUpdate = (path) => {
+    const [result, setResult] = useState();
+    const updateData = useCallback((value) => {
+      update(ref(database, path), value)
+      .then(() => setResult(makeResult()))
+      .catch((error) => setResult(makeResult(error)))
+    }, [database, path]);
+  
+    return [updateData, result];
+  };
